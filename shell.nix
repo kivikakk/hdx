a28c78b71dcb71125e6dff47367ddcaa7dcc71dc
Charlotte <charlotte@hrzn.ee>
2023-07-25 10:57:08 +1000

    shell.nix: restore "name" for amaranth dev mode.

--- shell.nix
+++ shell.nix
@@ -8,6 +8,8 @@ in
   if amaranth_dev_mode
   then
     hdx.amaranth.overridePythonAttrs ({nativeBuildInputs, ...}: {
+      name = "hdx-amaranth";
+
       src = null;
 
       nativeBuildInputs =
@@ -18,8 +20,6 @@ in
         # pipShellHook looks for pyproject.toml in cwd.
         cd dev/amaranth
       '';
-
-      # IN_NIX_SHELL_NAMED = "hdx-amaranth";
     })
   else
     hdx.pkgs.mkShell {
