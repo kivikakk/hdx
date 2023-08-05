ecaad88f2173bf8db2a91c49c36b7d2bccd91e09
Charlotte <charlotte@lottia.net>
2023-08-05 13:06:26 +1000

    nix fmt.

--- nix/python.nix
+++ nix/python.nix
@@ -1,10 +1,11 @@
-inputs@{...}:
-let
-  python = (inputs.python.override { includeSiteCustomize = false; }).overrideAttrs (finalAttrs: prevAttrs: {
-    postInstall = prevAttrs.postInstall + ''
-      # Override sitecustomize.py with our NIX_PYTHONPATH-preserving variant.
-      cp ${../patches/sitecustomize.py} $out/${finalAttrs.passthru.sitePackages}/sitecustomize.py
-    '';
+inputs @ {...}: let
+  python = (inputs.python.override {includeSiteCustomize = false;}).overrideAttrs (finalAttrs: prevAttrs: {
+    postInstall =
+      prevAttrs.postInstall
+      + ''
+        # Override sitecustomize.py with our NIX_PYTHONPATH-preserving variant.
+        cp ${../patches/sitecustomize.py} $out/${finalAttrs.passthru.sitePackages}/sitecustomize.py
+      '';
   });
 in
-  python.override { self = python; }
+  python.override {self = python;}
