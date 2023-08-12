563df239005563c58fc3c7edd8356864bfe1bc93
Charlotte <charlotte@lottia.net>
2023-08-12 13:43:09 +1000

    .#yosys-amaranth: hdx-out in yosys/, ignore that; document.

--- amaranth-dev-shell.nix
+++ amaranth-dev-shell.nix
@@ -13,10 +13,11 @@ in
       ++ (lib.filter (p: p != hdx.amaranth) (builtins.attrValues hdx.ours));
 
     preShellHook = ''
-      # setuptoolsShellHook looks for setup.py in cwd.  Identify where amaranth
-      # might be and cd to it.
+      set -e
       export HDX_START="$(pwd)"
 
+      # setuptoolsShellHook looks for setup.py in cwd.  Identify where amaranth
+      # might be and cd to it.
       local _found=0 _dir
       for _dir in . amaranth; do
         if grep -q 'name = "amaranth"' "$_dir/pyproject.toml"; then
@@ -26,10 +27,10 @@ in
         fi
       done
       if test "$_found" -eq 0; then
-        echo "ERROR: '$(pwd)' doesn't look like Amaranth root, and no"
-        echo "'amaranth' subdirectory was found."
-        echo "'nix develop hdx#amaranth' only works when I can find Amaranth,"
-        echo "otherwise I can't set up correctly."
+        echo "hdx: ERROR: '$(pwd)' doesn't look like Amaranth root, and no"
+        echo "hdx: 'amaranth' subdirectory was found."
+        echo "hdx: 'nix develop hdx#amaranth' only works when I can find Amaranth,"
+        echo "hdx: otherwise I can't set up correctly."
         exit 1
       fi
     '';
--- yosys-amaranth-dev-shell.nix
+++ yosys-amaranth-dev-shell.nix
@@ -26,37 +26,42 @@ in
       ++ hdx.yosys.buildInputs;
 
     preShellHook = ''
+      set -e
       export HDX_START="$(pwd)"
 
       local _found=0 _dir
       for _dir in .; do
         if test -d "$_dir/yosys" -a -d "$_dir/amaranth"; then
           cd "$_dir"
-          export HDX_OUT="$(pwd)/hdx-out"
           _found=1
           break
         fi
       done
 
       if test "$_found" -eq 0; then
-        echo "ERROR: '$(pwd)' doesn't contain 'yosys' and 'amaranth' directories."
-        echo "'nix develop hdx#yosys-amaranth' only works when executed with cwd"
-        echo "containing these, otherwise I can't set up correctly."
+        echo "hdx: ERROR: '$(pwd)' doesn't contain 'yosys' and 'amaranth' directories."
+        echo "hdx: 'nix develop hdx#yosys-amaranth' only works when executed with cwd"
+        echo "hdx: containing these, otherwise I can't set up correctly."
         exit 1
       fi
 
+      cd yosys
+
+      export HDX_OUT="$(pwd)/hdx-out"
       if ! test -d "$HDX_OUT"; then
-        echo "Running \`mkdir $HDX_OUT\` for Yosys install outputs."
+        echo "hdx: Running \`mkdir $HDX_OUT\` for Yosys install outputs."
         mkdir "$HDX_OUT"
       fi
+      export PATH="$HDX_OUT/bin:$PATH"
 
-      cd yosys
-      cat >Makefile.conf.hdx <<'EOF'
+      # Install a Makefile.conf that will compile Yosys correctly (and for debug),
+      # and install it where we expect.  Ensure we don't overwrite a user's own one.
+      cat >Makefile.conf.hdx <<-'EOF'
       ${(hdx.yosys.overrideAttrs {
           makefileConfPrefix = "$(HDX_OUT)";
           extraMakefileConf = ''
             ENABLE_DEBUG=1
-            STRIP=echo Not doing this: strip
+            STRIP=echo hdx: Not doing this: strip
           '';
         })
         .makefileConf}
@@ -64,13 +69,20 @@ in
       if test -f Makefile.conf; then
         if ! diff --color -u Makefile.conf Makefile.conf.hdx; then
           echo
-          echo "ERROR: '$(pwd)/Makefile.conf' exists and differs from what I want to install."
-          echo "Not doing any more.  '$(pwd)/Makefile.conf.hdx' is left in place."
+          echo "hdx: ERROR: '$(pwd)/Makefile.conf' exists and differs from what I want to install."
+          echo "hdx: Not doing any more.  '$(pwd)/Makefile.conf.hdx' is left in place."
           exit 1
         fi
       fi
       mv Makefile.conf.hdx Makefile.conf
-      export PATH="$HDX_OUT/bin:$PATH"
+
+      # Append the created output directory to .git/info/exclude so it doesn't
+      # make their Yosys checkout look dirty.
+      if ! grep -q hdx-out .git/info/exclude; then
+        echo "hdx: Appending '/hdx-out' to '$(pwd)/.git/info/exclude'."
+        echo /hdx-out >> .git/info/exclude
+      fi
+
       cd ..
 
       # setuptoolsShellHook looks for setup.py in cwd, so finish in amaranth/.
